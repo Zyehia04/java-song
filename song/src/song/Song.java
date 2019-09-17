dcdd0a8b2e3ad17fb16a3c379d6e75d9af2b91aa
21399 <21399@MININT-MPKMA8T.columbushs.com>
2019-09-17 10:41:30 -0400

    Done

--- song/src/song/Song.java
+++ song/src/song/Song.java
@@ -16,8 +16,25 @@ public class Song {
      */
     public static void main(String[] args) {
         // Code inside the main method
+        System.out.println("Shoutout VloneChino man you feel");
+        chorus();
+        System.out.println("I been flexin a patek, i ain't sleep yet\n" +
+"I be cashing selling wok send my beans west\n" +
+"All Y’all haters postin up send a weak text\n" +
+"And I know y’all is jealous cuz it’s me next");
+
+        chorus();
+        System.out.println("- AcogZxin (Genius.com)");
         
-            
+        
     }
     
+    public static void chorus(){
+        System.out.println();
+        System.out.println("Check my chopper and my jersey bruh it’s vlone\n" +
+"Haters crying downin tech tell em speak out\n" +
+"Rollin up a wood, cloud my vision see y’all\n" +
+"Said I pull up with the sauce, Dijon");
+        System.out.println();
+    }
 }
